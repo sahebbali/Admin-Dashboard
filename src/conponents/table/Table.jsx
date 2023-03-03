aed17a63a439c12db861d243676510b53b5cdb2a
sahebbali <sahebbali253@gmail.comm>
2023-03-03 02:01:17 -0800

    first commit

--- src/conponents/table/Table.jsx
+++ src/conponents/table/Table.jsx
@@ -1,4 +1,4 @@
-import "./table.scss"
+import "./table.scss";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,7 +7,8 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-const rows = [
+const List = () => {
+  const rows = [
     {
       id: 1143155,
       product: "Acer Nitro 5",
@@ -59,45 +60,43 @@ const rows = [
       status: "Pending",
     },
   ];
-const List=()=> {
   return (
     <TableContainer component={Paper} className="table">
-        <Table sx={{minWidth: 650}} aria-label="simple table">
-            <TableRow>
-                <TableCell className="tableCell">Tracking ID</TableCell>
-                <TableCell className="tableCell">Product</TableCell>
-                <TableCell className="tableCell">Customer</TableCell>
-                <TableCell className="tableCell">Date</TableCell>
-                <TableCell className="tableCell">Amount</TableCell>
-                <TableCell className="tableCell">Payment Method</TableCell>
-                <TableCell className="tableCell">Status</TableCell>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className="tableCell">Tracking ID</TableCell>
+            <TableCell className="tableCell">Product</TableCell>
+            <TableCell className="tableCell">Customer</TableCell>
+            <TableCell className="tableCell">Date</TableCell>
+            <TableCell className="tableCell">Amount</TableCell>
+            <TableCell className="tableCell">Payment Method</TableCell>
+            <TableCell className="tableCell">Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell className="tableCell">{row.id}</TableCell>
+              <TableCell className="tableCell">
+                <div className="cellWrapper">
+                  <img src={row.img} alt="" className="image" />
+                  {row.product}
+                </div>
+              </TableCell>
+              <TableCell className="tableCell">{row.customer}</TableCell>
+              <TableCell className="tableCell">{row.date}</TableCell>
+              <TableCell className="tableCell">{row.amount}</TableCell>
+              <TableCell className="tableCell">{row.method}</TableCell>
+              <TableCell className="tableCell">
+                <span className={`status ${row.status}`}>{row.status}</span>
+              </TableCell>
             </TableRow>
-            <TableBody>
-                {
-                    rows.map((row)=>(
-                        <TableRow className="tableCell" key={row.id}>
-                            <TableCell className="tableCell">
-                                <div className="cellWrapper">
-                                    <img src={row.img} alt="" className="image"/>
-                                    {row.product}
-                                </div>
-                            </TableCell>
-                            <TableCell className="tableCell">{row.customer}</TableCell>
-                            <TableCell className="tableCell">{row.date}</TableCell>
-                            <TableCell className="tableCell">{row.amount}</TableCell>
-                            <TableCell className="tableCell">{row.method}</TableCell>
-                            <TableCell className="tableCell">
-                                <span className={`status ${row.status}`}>{row.status}</span>
-                            </TableCell>
-
-                        </TableRow>
-                    ))
-                }
-            </TableBody>
-        </Table>
-
+          ))}
+        </TableBody>
+      </Table>
     </TableContainer>
-  )
-}
+  );
+};
 
-export default List ;
+export default List;
